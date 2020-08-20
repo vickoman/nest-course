01f2a2a31d15c6b13725219c3302881af0dcedd8
Victor Manuel <victor@dreamcode.io>
2020-08-20 17:04:43 -0500

    Setup 10% of course

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Param, Body, Post } from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+    @Get("flavors")
+    findAll() {
+        return "this action return all coffees";
+    }
+
+    @Get(":id")
+    findOne(@Param('id') id: string ) {
+        return `this action return #${id} coffee`;
+    }
+
+    @Post()
+    create(@Body() body) {
+        return body;
+    }
+}
