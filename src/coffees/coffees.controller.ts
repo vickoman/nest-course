0325a9eef607a3465dbd5f42ec4b783d7d4dc07c
Victor Manuel <victor@dreamcode.io>
2020-08-20 17:34:39 -0500

    Add pagination for large amount of data

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,19 +1,54 @@
-import { Controller, Get, Param, Body, Post } from '@nestjs/common';
+import { Controller, Get, Param, Body, Post, Patch, Delete, Query } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
+    /**
+     * Get all Coffees
+     */
     @Get("")
-    findAll() {
-        return "this action return all coffees";
+    findAll(@Query() paginationQuery) {
+        const { limit, offset } = paginationQuery;
+        return `this action return all coffees Limit: ${limit} and Offset: ${offset}`;
     }
 
+    /**
+     *
+     * @param id
+     * Get Cooffee by id
+     */
     @Get(":id")
     findOne(@Param('id') id: string ) {
         return `this action return #${id} coffee`;
     }
 
+    /**
+     *
+     * @param body
+     * Create new Coffee
+     */
     @Post()
     create(@Body() body) {
         return body;
+    }
+
+    /**
+     *
+     * @param id
+     * @param body
+     *  Patch it's use to update partial
+     */
+    @Patch(":id")
+    update(@Param("id") id: string, @Body() body) {
+        return `this action updates #${id} coffee`;
+    }
+
+    /**
+     *
+     * @param id
+     * Delete coffee
+     */
+    @Delete(":id")
+    remove(@Param("id") id: string) {
+        return `this actions remove #${id} coffee`;
     }
 }
